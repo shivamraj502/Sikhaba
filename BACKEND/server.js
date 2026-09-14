776b11b1a05dd5fee13163d3e3f842df32262f60
shivam_raj <shivamraj81020051@gmail.com>
2026-09-14 22:54:00 +0530

    server.js arranged according to execution

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -3,14 +3,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const { testConnection } = require('./config/db');
+const authRoutes = require('./modules/auth/auth.routes');
 
 const app = express();
 
-const authRoutes = require('./modules/auth/auth.routes');
-app.use('/api/auth', authRoutes);
-
 app.use(cors());
 app.use(express.json());
+app.use('/api/auth', authRoutes);
 
 // Health check route — confirms server is alive
 app.get('/api/health', (req, res) => {
