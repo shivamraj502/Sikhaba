d0ba56d5c4482817dcc18f2c19af6bb6416cf5a3
shivam_raj <shivamraj81020051@gmail.com>
2026-09-15 02:49:48 +0530

    server.js updated: roomRoutes

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -5,12 +5,14 @@ require('dotenv').config();
 const { testConnection } = require('./config/db');
 const authRoutes = require('./modules/auth/auth.routes');
 const authMiddleware = require('./middlewares/authMiddleware');
+const roomRoutes = require('./modules/rooms/room.routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/rooms', roomRoutes);
 
 // Health check route — confirms server is alive
 app.get('/api/health', (req, res) => {
