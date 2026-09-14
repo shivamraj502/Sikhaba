d35e0b673ba97f176cea8866568a9da4014d9119
shivam_raj <shivamraj81020051@gmail.com>
2026-09-15 02:56:26 +0530

    server.js updated: from app.listen to server.listen

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const http = require('http');  
 require('dotenv').config();
 
 const { testConnection } = require('./config/db');
 const authRoutes = require('./modules/auth/auth.routes');
 const authMiddleware = require('./middlewares/authMiddleware');
 const roomRoutes = require('./modules/rooms/room.routes');
+const initSocket = require('./sockets/index');  
 
 const app = express();
 
@@ -19,9 +21,12 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', message: 'Sikhaba backend is running' });
 });
 
+const server = http.createServer(app);                  
+initSocket(server);  
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {                        // ← changed from app.listen to server.listen
   console.log(`🚀 Server running on http://localhost:${PORT}`);
   await testConnection();
 });
