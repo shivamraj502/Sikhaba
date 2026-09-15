8afc138fa7dd6cfe89fa82ed408ceb8e2a0467c3
shivam_raj <shivamraj81020051@gmail.com>
2026-09-15 09:12:01 +0530

    auth.service.js updated : added admin

--- BACKEND/modules/auth/auth.service.js
+++ BACKEND/modules/auth/auth.service.js
@@ -5,9 +5,16 @@ const { pool } = require('../../config/db');
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = '7d';
 
+// function generateToken(user) {
+//   return jwt.sign(
+//     { id: user.id, email: user.email, phone: user.phone },
+//     JWT_SECRET,
+//     { expiresIn: JWT_EXPIRES_IN }
+//   );
+// }
 function generateToken(user) {
   return jwt.sign(
-    { id: user.id, email: user.email, phone: user.phone },
+    { id: user.id, email: user.email, phone: user.phone, role: user.role },   
     JWT_SECRET,
     { expiresIn: JWT_EXPIRES_IN }
   );
