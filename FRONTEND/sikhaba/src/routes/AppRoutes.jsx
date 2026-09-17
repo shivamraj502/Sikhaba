e3d93f713c8e4a170e900a4af39b8c646dc80ae6
shivam_raj <shivamraj81020051@gmail.com>
2026-09-17 21:06:23 +0530

    appRoutes.jsx updated

--- FRONTEND/sikhaba/src/routes/AppRoutes.jsx
+++ FRONTEND/sikhaba/src/routes/AppRoutes.jsx
@@ -1,26 +1,3 @@
-// import { Routes, Route, Navigate } from 'react-router-dom';
-// import Login from '../pages/Auth/Login';
-// import Signup from '../pages/Auth/Signup';
-// import OtpVerify from '../pages/Auth/OtpVerify';
-// import Home from '../pages/Home/Home';
-// import { useAuth } from '../context/AuthContext';
-
-// function AppRoutes() {
-//   const { user } = useAuth();
-
-//   return (
-//     <Routes>
-//       <Route path="/login" element={<Login />} />
-//       <Route path="/signup" element={<Signup />} />
-//       <Route path="/otp-login" element={<OtpVerify />} />
-//       <Route path="/home" element={user ? <Home /> : <Navigate to="/login" />} />
-//       <Route path="/" element={<Navigate to={user ? '/home' : '/login'} />} />
-//     </Routes>
-//   );
-// }
-
-// export default AppRoutes;
-
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Auth/Login';
 import Signup from '../pages/Auth/Signup';
