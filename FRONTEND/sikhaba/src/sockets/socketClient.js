e670053892aa5f08bc98e69221c37c9788c13d01
shivam_raj <shivamraj81020051@gmail.com>
2026-09-18 01:05:36 +0530

    socketClient.js updated

---
+++ FRONTEND/sikhaba/src/sockets/socketClient.js
@@ -0,0 +1,24 @@
+import { io } from 'socket.io-client';
+
+let socket = null;
+
+export function connectSocket(token) {
+  if (socket) return socket;
+
+  socket = io('http://localhost:5000', {
+    auth: { token }
+  });
+
+  return socket;
+}
+
+export function getSocket() {
+  return socket;
+}
+
+export function disconnectSocket() {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+}
